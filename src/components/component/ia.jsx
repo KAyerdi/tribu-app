e32999994b61e51f2f91ddb906c6da7b977a4ff1
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-01-12 16:09:00 -0300

    Actualizando

--- src/components/component/ia.jsx
+++ src/components/component/ia.jsx
@@ -180,9 +180,8 @@ export function IA() {
           Reservar
         </button>
       </form>
-        <div className='mt-6  text-center text-3xl font-extrabold text-gray-900'>
+        <div className='mt-6 text-center text-3xlfont-extrabold text-gray-900'>
         <h2>Donde nos encontras ?</h2>
-        <h2>Malvinas Argentinas 240. Entre calle Los Alamos y Rebagliatti</h2>
         </div>
         <div>
         <iframe
