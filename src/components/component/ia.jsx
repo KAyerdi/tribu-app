23da0db513bf8ceea3205733825ce918b5f9b919
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-01-12 12:00:06 -0300

    estilos cards

--- src/components/component/ia.jsx
+++ src/components/component/ia.jsx
@@ -29,8 +29,10 @@ export function IA() {
         </h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 text-black'>
           <Card>
-
-            <Image src="/1.jpeg" alt="Dog 1" width={260} height={200} />
+            <Image src="/1.jpeg" alt="Dog 1" style={{
+                aspectRatio: "200/200",
+                objectFit: "cover",
+              }} />
             <CardContent className='mt-5'>
               <CardTitle> Hook </CardTitle>
               <CardDescription className='mt-2'>
@@ -38,8 +40,12 @@ export function IA() {
               </CardDescription>
             </CardContent>
           </Card>
+
           <Card>
-          <Image src="/2.jpeg" alt="Dog 1" width={260} height={200} />
+          <Image src="/2.jpeg" alt="Dog 2" style={{
+                aspectRatio: "200/200",
+                objectFit: "cover",
+              }} />
             <CardContent className='mt-5'>
               <CardTitle>Panchi</CardTitle>
               <CardDescription className='mt-2'>
@@ -47,8 +53,12 @@ export function IA() {
               </CardDescription>
             </CardContent>
           </Card>
+
           <Card>
-          <Image src="/3.jpeg" alt="Dog 1" width={260} height={200} />
+          <Image src="/3.jpeg" alt="Dog 3"style={{
+                aspectRatio: "200/200",
+                objectFit: "cover",
+              }} />
             <CardContent className='mt-5'>
               <CardTitle>Florinda</CardTitle>
               <CardDescription className='mt-2'>
@@ -56,8 +66,12 @@ export function IA() {
               </CardDescription>
             </CardContent>
           </Card>
+
           <Card>
-          <Image src="/4.jpeg" alt="Dog 1" width={260} height={200} />
+          <Image src="/4.jpeg" alt="Dog 4" style={{
+                aspectRatio: "200/200",
+                objectFit: "cover",
+              }} />
             <CardContent className='mt-5'>
               <CardTitle>Milka</CardTitle>
               <CardDescription className='mt-2'>
@@ -68,13 +82,13 @@ export function IA() {
         </div>
       </section>
 
-      < className='max-w-md w-full space-y-8 mt-8 rounded-lg shadow-lg p-6'>
+      <div className='max-w-md w-full space-y-8 mt-8 rounded-lg shadow-lg p-6'>
         <div>
           <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
             Reserva tu cita para el cuidado de tu mascota
           </h2>
         </div>
-        <form className='mt-8 space-y-6'>
+        < className='mt-8 space-y-6'>
           <div className='rounded-md shadow-sm -space-y-px'>
             <div>
               <label className='sr-only' htmlFor='name'>
@@ -142,7 +156,7 @@ export function IA() {
               />
             </div>
           </div>
-          <div>
+          <>
             <button
               className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
               type='submit'
@@ -151,6 +165,7 @@ export function IA() {
             </button>
           </div>
         </form>
+
         <div>
           <iframe
             src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d412.0700896637636!2d-60.2494795679535!3d-34.28525239344473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b90dc93555b4a1%3A0x8e7c2ed78d0ccebf!2sTRIBU%20Forrajeria!5e0!3m2!1ses!2sar!4v1705064812927!5m2!1ses!2sar'
@@ -162,14 +177,16 @@ export function IA() {
             referrerpolicy='no-referrer-when-downgrade'
           ></iframe>
         </div>
-        
+
         <div className="flex justify-center space-x-4 mt-4">
           <Link className="text-blue-600 hover:text-blue-800" href="#">
             <i className="fab fa-facebook-square text-3xl" />
           </Link>
+
           <Link className="text-pink-600 hover:text-pink-800" href="#">
             <i className="fab fa-instagram-square text-3xl" />
           </Link>
+
           <Link className="text-green-600 hover:text-green-800" href="#">
             <i className="fab fa-whatsapp-square text-3xl" />
           </Link>
