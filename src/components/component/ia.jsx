022313d427330970ef049fb7d38f5be1030ae3a3
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-01-12 15:22:50 -0300

    actualizando

--- src/components/component/ia.jsx
+++ src/components/component/ia.jsx
@@ -82,13 +82,13 @@ export function IA() {
         </div>
       </section>
 
-      <div className='max-w-md w-full space-y-8 mt-8 rounded-lg shadow-lg p-6'>
+      <form className='max-w-md w-full space-y-8 mt-8 rounded-lg shadow-lg p-6'>
         <div>
           <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
             Reserva tu cita para el cuidado de tu mascota
           </h2>
         </div>
-        < className='mt-8 space-y-6'>
+        < form className='mt-8 space-y-6'>
           <div className='rounded-md shadow-sm -space-y-px'>
             <div>
               <label className='sr-only' htmlFor='name'>
@@ -156,15 +156,13 @@ export function IA() {
               />
             </div>
           </div>
-          <>
             <button
               className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
               type='submit'
             >
               Reservar
             </button>
-          </div>
-        </form>
+      </form>
 
         <div>
           <iframe
