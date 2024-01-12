8fe8d38c3f830067273349f936fb69a531722c62
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-01-12 15:55:39 -0300

    dando estilo a los titulos

--- src/components/component/ia.jsx
+++ src/components/component/ia.jsx
@@ -181,8 +181,10 @@ export function IA() {
         </button>
       </form>
       <div>
+        <div className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
         <h1>Donde nos encontras ?</h1>
         <h2>Malvinas Argentinas 240. Entre calle Los Alamos y Rebagliatti</h2>
+        </div>
         <iframe
           src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d412.0700896637636!2d-60.2494795679535!3d-34.28525239344473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b90dc93555b4a1%3A0x8e7c2ed78d0ccebf!2sTRIBU%20Forrajeria!5e0!3m2!1ses!2sar!4v1705064812927!5m2!1ses!2sar'
           width='600'
